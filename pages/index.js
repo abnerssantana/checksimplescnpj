da38f65fe545ab19ab17dc3f7d62994c85a5cd94
Abner Santana <abnerss@outlook.com>
2024-07-04 17:16:32 -0300

    h-dvh

--- pages/index.js
+++ pages/index.js
@@ -82,16 +82,7 @@ export default function Home() {
     };
 
     return (
-        <div className="relative h-full overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-32">
-            <div className="absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6">
-                <div
-                    style={{
-                        clipPath:
-                            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                    }}
-                    className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
-                />
-            </div>
+        <div className="relative h-dvh overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-32">
             <div className="mx-auto max-w-9xl px-6 lg:px-8">
                 <div className="mb-4">
                     <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl text-center mb-8">
@@ -111,17 +102,18 @@ export default function Home() {
                         >
                             {loading ? 'Buscando...' : 'Buscar CNPJs'}
                         </button>
-                    </div>
-                    {results.length > 0 && (
+                        {results.length > 0 && (
                         <div className="flex justify-center mb-8">
                             <button
                                 onClick={exportToCsv}
-                                className="rounded-md bg-green-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-500"
+                                className="rounded-md ml-4 bg-green-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-500"
                             >
                                 Exportar Resultados para CSV
                             </button>
                         </div>
                     )}
+                    </div>
+                    
                     {error && (
                         <p className="text-white text-center mt-6">
                             Erro ao processar o arquivo CSV: {error}
